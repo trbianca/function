4ca34bfbddcddc248b04bced1b0ed18becd041fa
trbianca <trbianca@outlook.com>
2022-01-20 16:27:24 -0300

    MOD2 - Desafio 04 Function

---
+++ script.js
@@ -0,0 +1,37 @@
+//1 - crie uma função que exiba uma mensagem no console
+
+// function nome(){
+//     console.log("Função nome")
+// }
+// nome();
+
+//2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
+
+// function nomeParametro(nome){
+//  console.log(`Função com parâmetro: ${nome}`)
+// }
+// nomeParametro("Valor do Parâmetro")
+
+//3 - crie uma função que receba nome, idade, e um estilo musical preferido (parâmetros) e exiba no console
+
+// function inf(nome, idade, estiloMusical){
+//     console.log(`Nome: ${nome}. Idade: ${idade}. Estilo Musical: ${estiloMusical}`);
+// }
+// inf("Bianca", 21, "Pop")
+
+//4 - crie uma função que receba um filme, uma música (parâmetros) e exiba no console
+
+// function inf(filme, musica){
+//     console.log(`Filme: ${filme}. Música: ${musica}`);
+// }
+
+// inf("Cruella", "Carefully- Demi Lovato")
+
+//*Bônus:
+
+//5 - crie uma função que retorne o triplo do número recebido no parâmetro da função
+
+// function num (numero){
+//     console.log( numero * 3);
+// }
+// num(50);
